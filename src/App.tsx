3f3459e43882e788c25bf0cefe15fcc6afae626c
Tamtikorn <gan_zaza_12@hotmail.com>
2023-11-22 14:18:21 +0700

    add by gan

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>SAIG Git Training</h1>
+      <h1>Hacked By Gan-ISUS</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
