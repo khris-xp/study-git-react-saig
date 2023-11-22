d93438ecd6dfbf3d17f7cd84d2a081d94e1f6127
khris-xp <khrisbharmmano@gmail.com>
2023-11-22 14:17:40 +0700

    update branch khris-xp

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0)
 
   return (
     <>
@@ -28,6 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        Hello from khris-xp
+      </div>
     </>
   )
 }
