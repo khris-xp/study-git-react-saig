edb0386436f09aabf00c8917a01bf8e78b9e7808
pawito236 <pawito236@gmail.com>
2023-11-22 14:28:38 +0700

    update by wit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hacked By Gan-ISUS</h1>
+      <h1>Hacked By SAIG</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,13 +26,14 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more 555
+        Ctrl +Click on the Vite and React logos to learn more 555
       </p>
       <p className="my-doc">
         This is pawit branch
       </p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/3/35/KMITL_PCC.png" alt="logo" />
       <div>
-        Hello from khris-xp
+        Hi from khris-xp
       </div>
     </>
   )
