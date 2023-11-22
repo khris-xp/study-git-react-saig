d832d906d049c5a69159c98652314175d7c1c543
pawito236 <pawito236@gmail.com>
2023-11-22 15:44:14 +0700

    feat(App): Add change bg color btn and form

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState<number>(0)
-  const [clicked, setClicked] = useState<boolean>(false);
+  const [count, setCount] = useState(0)
+  const [clicked, setClicked] = useState(false)
+  const [name, setName] = useState('')
+  const [bgColor, setBgColor] = useState('#ffffff')
 
-  function handleCount(): void {
-    setCount((count) => count + 1);
+  function handleCount() {
+    setCount(count + 1)
   }
 
-  function handleClick(): void {
-    setClicked(!clicked);
+  function handleClick() {
+    setClicked(!clicked)
+  }
+
+  function handleNameChange(event) {
+    setName(event.target.value)
+  }
+
+  function toggleBgColor() {
+    setBgColor(bgColor === '#ffffff' ? '#f2f2f2' : '#ffffff')
   }
 
   return (
     <Fragment>
+
       <div className='logos'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo spin" alt="Vite logo" />
@@ -27,6 +38,7 @@ function App() {
       </div>
       <h1 className='spin'>Hacked By Gan-ISUS</h1>
       <h2>China number one</h2>
+
       <div className="card">
         <button onClick={() => handleCount()}>
           count is {count}
@@ -36,6 +48,9 @@ function App() {
         </p>
         <br />
         <hr />
+        <div>
+          <button onClick={toggleBgColor}>Change Background Color</button>
+        </div>
       </div>
       <p className="read-the-docs">
         Ctrl +Click on the Vite and React logos to learn more 555
@@ -50,6 +65,24 @@ function App() {
       <div>
         <button onClick={() => handleClick()}>{clicked ? "I have Clicked" : "Clicked"}</button>
       </div>
+
+      <form>
+        <label htmlFor='name'>Enter your name:</label>
+        <input type='text' id='name' value={name} onChange={handleNameChange} />
+      </form>
+
+      <style>
+        {`
+          #root {
+            max-width: 1280px;
+            margin: 0 auto;
+            padding: 2rem;
+            text-align: center;
+            background-color: ${bgColor};
+          }
+          /* Existing CSS styles... */
+        `}
+      </style>
     </Fragment>
   )
 }
